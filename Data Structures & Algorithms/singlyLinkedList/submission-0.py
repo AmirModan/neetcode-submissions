05fefaa158636ae4d0205cdc54de757fbcbccd5a
AmirModan <52586334+AmirModan@users.noreply.github.com>
2026-05-04 13:35:15 -0700

    Add: singlyLinkedList - submission-0

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-0.py
@@ -0,0 +1,26 @@
+class LinkedList:
+    
+    def __init__(self):
+        self.ll = []
+    
+    def get(self, index: int) -> int:
+        if len(self.ll) > index:
+            return self.ll[index]
+        else:
+            return -1
+
+    def insertHead(self, val: int) -> None:
+        self.ll.insert(0, val)
+
+    def insertTail(self, val: int) -> None:
+        self.ll.append(val)
+
+    def remove(self, index: int) -> bool:
+        if len(self.ll) > index:
+            self.ll.pop(index)
+            return True
+        else:
+            return False
+
+    def getValues(self) -> List[int]:
+        return self.ll
